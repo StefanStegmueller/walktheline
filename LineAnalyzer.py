0c362a1b4fc3ad5f7f1f6d9aba299c73457b4555
Stefan Stegmüller <Stefan.Steg@gmail.com>
2016-11-03 17:58:26 +0100

    -enh: tried implementing oop

---
+++ LineAnalyzer.py
@@ -0,0 +1,48 @@
+import numpy
+import cv2
+
+class LineAnalyzer:
+
+    def __init__(self):
+        return
+
+    def turn_img(img):
+        width = img.shape[0]
+        height = img.shape[1] #height and width are switched
+        M = cv2.getRotationMatrix2D((height / 2, width / 2), 180, 1)
+        turned_img = cv2.warpAffine(img, M, (height, width))
+        return turned_img
+
+    def print_image(img):
+        cv2.imshow('img', img)
+        cv2.waitKey(0)
+        return
+
+    def crop_roi(img, start_x, start_y, width, height):
+        turned_img = turn_img(img)
+        resized_turned_img = turned_img[start_y:height, start_x:width]
+        roi = turn_img(resized_turned_img)
+        return roi
+
+    def main(self):
+        imgSource = '2016-05-14-04.08.10.bmp'
+
+        img = cv2.imread(imgSource, 0)
+
+        height = img.shape[0]
+        width = img.shape[1]
+
+        start_x = 0
+        start_y = height - (height * 0.99)
+        new_w = width
+        new_h = height - (height * 0.9)
+
+        roi = self.crop_roi(img, start_x, start_y, new_w, new_h)
+
+        self.print_image(roi)
+        return
+
+    if __name__ == "__main__":
+        main()
+
+
