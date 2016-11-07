a201f025761e3ceef82f9074e3a803b0766ab249
Stefan Stegmüller <Stefan.Steg@gmail.com>
2016-11-07 11:46:11 +0100

    -add: implemented contour detection

--- LineAnalyzer.py
+++ LineAnalyzer.py
@@ -3,6 +3,7 @@ import cv2
 
 class LineAnalyzer:
 
+    #turns image 180 degrees
     def turn_img(self, img):
         width = img.shape[0]
         height = img.shape[1] #height and width are switched
@@ -21,10 +22,15 @@ class LineAnalyzer:
         roi = self.turn_img(resized_turned_img)
         return roi
 
+    def find_conturs(self, roi):
+        ret, thresh = cv2.threshold(roi, 127, 255, 0)
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        return contours, hierarchy
+
     def analyze(self):
         imgSource = '2016-05-14-04.08.10.bmp'
 
-        img = cv2.imread(imgSource, 0)
+        img = cv2.imread(imgSource, 0) #reads greyscale image
 
         height = img.shape[0]
         width = img.shape[1]
@@ -35,6 +41,17 @@ class LineAnalyzer:
         new_h = height - (height * 0.9)
 
         roi = self.crop_roi(img, start_x, start_y, new_w, new_h)
+
+        gray = cv2.bilateralFilter(roi, 11, 17, 17)
+        edged = cv2.Canny(gray, 30, 200)
+
+
+        self.print_image(edged)
+        cv2.waitKey(0)
+
+        contours, hierarchy = self.find_conturs(edged)
+
+        cv2.drawContours(roi, contours, -1, (0, 255, 0), 3)
 
         self.print_image(roi)
         return
