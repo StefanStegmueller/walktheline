b217695c17f81d879ef6718f0b3034a283fa07f1
GordonShumway <Stefan.Steg@gmail.com>
2017-03-21 18:40:03 +0100

    recommit

---
+++ pyScripts/HttpService.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+class HttpService:
+
+    def __init__(self):
+        self.json_data = None
+        self.direction = None
+        self.wait_for_manual_instruction = None
+
+    def set_data(self, roi_position, roi_height, path_position, on_track, wait_for_manual_instruction):
+        self.json_data = {
+            "roi_position": roi_position,
+            "roi_height": roi_height,
+            "path_position": path_position,
+            "path_width": 0,
+            "on_track": on_track,
+            "wait_for_manual_instruction": wait_for_manual_instruction
+        }
+
+    def send_data(self, url, file):
+        files = {'file': open(file)}
+        response = requests.post(url, files=files, data=self.json_data)
+        print response.text
+        data = json.dumps(response.text)
+        self.direction = data["direction"]
+        self.wait_for_manual_instruction = data["wait_for_manual_instruction"]
